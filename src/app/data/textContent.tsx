4ab432b9ade2022ef3848273f84669ea71750a47
sinkpig <sinkpig00@gmail.com>
2026-06-12 17:15:31 -0300

    feat: added first section

---
+++ src/app/data/textContent.tsx
@@ -0,0 +1,10 @@
+export const textContent = {
+  home: {
+    heading: 'Portfolio',
+    titles: ['Frontend', 'UI Design'],
+    description: "Hello world! Milo here. I'm a Front end Designer based in 🇧🇷 Rio de Janeiro with background in UI Design and Graphic Design.",
+    link: 'mailto:sinkpig00@gmail.com',
+    linkText: 'Want to get in touch?',
+    message: 'Scroll right',
+  }
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,34 @@
-import Image from "next/image";
+import React from 'react';
+import Link from 'next/link'
+import { textContent } from './data/textContent';
+import styles from '@/app/page.module.css'
 
 export default function Home() {
-
+  const t = textContent.home
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="w-full max-w-5xl items-center justify-between lg:flex">
-        <p className="font-figtree flex w-full justify-center bg-blue/25 text-blue border border-blue rounded from-zinc-200 pb-6 pt-8 dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:w-auto lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Milo Ramirez Portfolio
-        </p>
-        <div className="flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="/"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src="/favicon.png"
-              alt="Milo Ramirez Logo"
-              width={50}
-              height={50}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="font-space_mono w-full flex flex-1 justify-center items-center">
-        <div>
-          <h1 className="w-full text-6xl pb-4">Hello World!</h1>
-          <div className="w-full flex justify-center space-x-4">
-            {['bg-purple', 'bg-red', 'bg-yellow', 'bg-blue'].map((bgColor, index) => (
-              <span key={index} className={`text-xl ${bgColor} dark:${bgColor}-light rounded-full px-4 py-2`}>{index + 1}</span>
-            ))}
+    <main className={styles.outerWrapper}>
+      <div className={styles.wrapper}>
+        <div className={styles.content}>
+          <div className={styles.introduction}>
+            <h2 className={styles.heading}>{t.heading}</h2>
+            <h1 className={styles.title}>
+              {t.titles.map((title, index) => (
+                <React.Fragment key={title}>
+                  <span>{title}</span>
+                  {t.titles.length > 1 && index < t.titles.length - 1 && (
+                    <span>&nbsp;&&nbsp;</span>
+                  )}
+                </React.Fragment>
+              ))}
+            </h1>
+            <h3 className={styles.description}>
+              {t.description}
+              &nbsp;<Link href={t.link}>{t.linkText}</Link>
+            </h3>
+            <div className={styles.message}>
+              {t.message}
+              <span></span>
+            </div>
           </div>
         </div>
       </div>
